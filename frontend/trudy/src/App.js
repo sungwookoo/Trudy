dc9474a2252eaedc4d7253bb9d9ac874b2c992cd
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-01-26 01:23:34 +0900

    feat : CORS 설정 파일 및 API 사용 예제 작성

--- frontend/trudy/src/App.js
+++ frontend/trudy/src/App.js
@@ -1,7 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // 요청받은 정보를 담아줄 변수 선언
+  const [testStr, setTestStr] = useState("");
+  const [test2api, setTestStr2] = useState("");
+
+  // 변수 초기화
+  function callback(str) {
+    setTestStr(str);
+  }
+
+  function callback2(str) {
+    setTestStr2(str);
+  }
+
+  // 첫 번째 렌더링을 마친 후 실행
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((Response) => {
+        callback(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("/api/test2")
+      .then((Response) => {
+        callback2(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,8 +52,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
