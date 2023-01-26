c935159a8f0f970e685ff5557e79cff03168998d
EHCha <chaeh0926@gmail.com>
2023-01-26 15:38:17 +0900

    Fix setProxy, App.js

--- frontend/trudy/src/App.js
+++ frontend/trudy/src/App.js
@@ -3,8 +3,48 @@ import { BrowserRouter, Routes, Route,} from 'react-router-dom';
 import Scroll from './Landing/Scroll';
 import Profile from './Profile/Profile';
 import Nav from './Common/Nav';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  // 요청받은 정보를 담아줄 변수 선언
+  const [testStr, setTestStr] = useState("");
+  const [test2api, setTestStr2] = useState("");
+
+  // 변수 초기화
+  function callback(str) {
+    setTestStr(str);
+  }
+
+  function callback2(str) {
+    setTestStr2(str);
+  }
+
+  // 첫 번째 렌더링을 마친 후 실행
+  useEffect(() => {
+    axios
+      .get("/api/test")
+      .then((Response) => {
+        callback(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get("/api/test2")
+      .then((Response) => {
+        callback2(Response.data);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, []);
+
+  // 위로 예시
+
     return (
       <div className="App">
         <BrowserRouter>
