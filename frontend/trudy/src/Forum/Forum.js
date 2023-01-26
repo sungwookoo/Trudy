deb81d1c067502e979f765d7e2592b9c5d504732
cheon2308 <cjsgusxo@gmail.com>
2023-01-27 04:03:40 +0900

    f-apiTest

--- frontend/trudy/src/Forum/Forum.js
+++ frontend/trudy/src/Forum/Forum.js
@@ -1,12 +1,39 @@
-import '../Forum/Forum.css'
+import "../Forum/Forum.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
-function Forum(props) {
-    return(
-        <div className='unfinished-headers'>
-            Forum Underconstruction
-            <img src='https://img.freepik.com/premium-vector/construction-worker-characters-with-vest-hard-hat_227564-320.jpg?w=2000' alt='ForumCon'></img>
-        </div>
-    )
+function Forum() {
+    const [postInfo, setPostInfo] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("http://sungwoo.shop:8080/api/posts/list").then((res) => {
+        setPostInfo(res.data);
+    });
+  }, []);
+  console.log(postInfo)
+  return (
+    <div>
+      {postInfo.map((post, i) => {
+      return (
+          <div className="postList">
+            <h4>title: {post.title}</h4>
+            <p>content: {post.content}</p>
+            <p>thumbnail_image_id: {post.thumbnail_image_id}</p>
+            <button onClick={()=>{
+              let copy = [...postInfo];
+              copy.splice(i, 1);
+              setPostInfo(copy)
+            }}>삭제</button>
+          </div>
+        );
+      })}
+      <br />
+      <br />
+      <br />
+      {/* <div>{props.object.area.busan}</div> */}
+    </div>
+  );
 }
 
 export default Forum;
