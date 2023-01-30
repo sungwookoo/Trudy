fd22284f19548addc0879a6b4343686ebeda4d58
EHCha <chaeh0926@gmail.com>
2023-01-30 14:56:09 +0900

    Make: Profile

--- frontend/trudy-ts/src/Account/Login.tsx
+++ frontend/trudy-ts/src/Account/Login.tsx
@@ -11,7 +11,7 @@ function Login() {
             <div>
                 <img
                 className="mx-auto h-12 w-auto"
-                src="faviconTrudy.png"
+                src="faviconTrudy.png" alt='login-logo'
                 />
                 <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                 Sign in to your account
--- frontend/trudy-ts/src/App.tsx
+++ frontend/trudy-ts/src/App.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route,} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Landing from './Landing/Landing';
 import TrudyMap from './TrudyMap/TrudyMap';
 import Profile from './Profile/Profile';
 import Nav from './Common/Nav';
 import Login from './Account/Login';
+import ProfileUpdateDefault from './Profile/ProfileUpdate';
 
 function App() {
   return (
     <div>
       {/* 네비게이션바 */}
-      <Nav />
+        <Nav />
         <Routes>
           <Route path='/' element={<Landing />} />
           <Route path='/trudymap' element={<TrudyMap />} />
           <Route path='/profile' element={<Profile />} />
           <Route path="/login" element={<Login/>} />
+          <Route path="/profileupdate" element={<ProfileUpdateDefault/>} />
         </Routes>
+      
   </div>
   );
 }
