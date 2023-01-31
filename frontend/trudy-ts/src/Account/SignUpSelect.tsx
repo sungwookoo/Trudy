af8245c9af967df8ce205f4428b83d2213816984
WChan <baewc95@gmail.com>
2023-01-31 15:06:16 +0900

    Feat : Planner

--- frontend/trudy-ts/src/Account/SignUpSelect.tsx
+++ frontend/trudy-ts/src/Account/SignUpSelect.tsx
@@ -23,6 +23,8 @@ function SignUpSelect() {
         <form className="mt-8 space-y-6" action="#" method="POST">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="-space-y-px rounded-md shadow-sm">
+
+            {/* 이메일 주소 */}
             <div>
               <label htmlFor="email-address" className="sr-only">
                 Email address
@@ -37,42 +39,16 @@ function SignUpSelect() {
                 placeholder="Email address"
               />
             </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password
-              </label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Password"
-              />
-            </div>
-            <div>
-              <label htmlFor="password" className="sr-only">
-                Password Confirm
-              </label>
-              <input
-                id="password-confirm"
-                name="password-confirm"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                placeholder="Password Confirm"
-              />
-            </div>
           </div>
+
+          {/* 이메일 인증 버튼 */}
           <div>
             <button
               type="submit"
-              className="group relative flex w-full justify-center rounded-md border border-transparent bg-trudy py-2 px-4 text-sm font-bold text-black hover:bg-trudy-dark focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="group relative flex w-full justify-center rounded-md border border-transparent bg-trudy-dark1 py-2 px-4 text-sm font-bold text-black hover:bg-trudy-dark2 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
             >
               <span className="absolute inset-y-0 left-0 flex items-center pl-3"></span>
-              Continue
+              Verify
             </button>
           </div>
         </form>
--- frontend/trudy-ts/src/Common/Nav.tsx
+++ frontend/trudy-ts/src/Common/Nav.tsx
@@ -1,62 +1,73 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Nav.css';
-import trudylogo from '../Assets/trudylogo.png';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Nav.css";
+import trudylogo from "../Assets/trudylogo.png";
 
 const Nav = () => {
-    return(
-        // 네비게이션바
+  return (
+    // 네비게이션바
     <nav className="nav-bar">
-    <NavLink to='/'>
-    <img className="nav-logo" src={trudylogo} alt="TrudyLogo"></img>
-    </NavLink>
+      <NavLink to="/">
+        <img className="nav-logo" src={trudylogo} alt="TrudyLogo"></img>
+      </NavLink>
+
       {/* 랜딩페이지 */}
       <div className="nav-item-logo">
-        <NavLink className="nav-link" to='/'>
+        <NavLink className="nav-link" to="/">
           Trudy
         </NavLink>
       </div>
+
       {/* 지도 */}
       <div className="nav-item">
-        <NavLink className="nav-link" to='/map'>
+        <NavLink className="nav-link" to="/map">
           Map
         </NavLink>
       </div>
+
       {/* 포럼 */}
       <div className="nav-item">
-        <NavLink className="nav-link" to='/forum'>
+        <NavLink className="nav-link" to="/forum">
           Forum
         </NavLink>
-      </div >
+      </div>
+
       {/* 스퀘어 */}
       <div className="nav-item">
-        <NavLink className="nav-link" to='/square'>
+        <NavLink className="nav-link" to="/square">
           Square
         </NavLink>
       </div>
+
       {/* 플래너 */}
       <div className="nav-item">
-        <NavLink className="nav-link" to='/planner'>
+        <NavLink className="nav-link" to="/planner">
           Planner
         </NavLink>
       </div>
+
       {/* 프로필 */}
       <div className="nav-item">
-        <NavLink className="nav-link" to='/profile'>
+        <NavLink className="nav-link" to="/profile">
           Profile
         </NavLink>
       </div>
+
       {/* 로그인 */}
-      <div className='nav-item'>
-        <NavLink className="nav-link" to='/signIn'>
+      <div className="nav-item">
+        <NavLink className="nav-link" to="/signin">
           Sign In
         </NavLink>
       </div>
-  </nav>
-)
-}
+
+      {/* 회원가입 */}
+      <div className="nav-item">
+        <NavLink className="nav-link" to="/signupselect">
+          Sign Up
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
-
-
-
