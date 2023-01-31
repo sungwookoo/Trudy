71978bd70eb95f660486f651fae60e59672a56b4
cheon2308 <cjsgusxo@gmail.com>
2023-01-31 17:23:05 +0900

    Feat : add English name in Map

--- frontend/trudy/src/TrudyMap/TrudyMap.tsx
+++ frontend/trudy/src/TrudyMap/TrudyMap.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import "./TrudyMap.css";
+import SearchBar from "./SearchBar";
 
 const containerStyle = {
   width: "1200px",
@@ -14,12 +16,17 @@ const center = {
 class TrudyMap extends Component {
   render() {
     return (
-      <LoadScript googleMapsApiKey="AIzaSyCr_VXyq_r6dte_29ocp-T2i6yf30VvUMI">
-        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          {/* Child components, such as markers, info windows, etc. */}
-          <></>
-        </GoogleMap>
-      </LoadScript>
+      <>
+        <SearchBar />
+        <LoadScript googleMapsApiKey="AIzaSyCr_VXyq_r6dte_29ocp-T2i6yf30VvUMI" region="US" language="en">
+          <div className="google-map">
+            <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+              {/* Child components, such as markers, info windows, etc. */}
+              <></>
+            </GoogleMap>
+          </div>
+        </LoadScript>
+      </>
     );
   }
 }
