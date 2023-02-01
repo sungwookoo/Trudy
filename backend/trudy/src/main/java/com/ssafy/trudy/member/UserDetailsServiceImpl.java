0d13deeb5c70463be8df811062d589c9d49396a5
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-02-02 05:13:45 +0900

    Feat : SpringSecurity JWT 적용

---
+++ backend/trudy/src/main/java/com/ssafy/trudy/member/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ssafy.trudy.member;
+
+import com.ssafy.trudy.member.model.Member;
+import com.ssafy.trudy.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public Member loadUserByUsername(String email) {
+        return Optional.ofNullable(memberRepository.findByEmail(email)).orElseThrow(() -> new BadCredentialsException("이메일 주소를 확인해주세요."));
+    }
+
+}
