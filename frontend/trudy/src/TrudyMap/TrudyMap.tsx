fb8c276a044003135bb6a761cd1c40fc47733599
cheon2308 <cjsgusxo@gmail.com>
2023-02-02 23:15:46 +0900

    Fix : center error

--- frontend/trudy/src/TrudyMap/TrudyMap.tsx
+++ frontend/trudy/src/TrudyMap/TrudyMap.tsx
@@ -1,41 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 import "./TrudyMap.css";
 import Place from "./Place";
 
 const containerStyle = {
-  width: "1000px",
-  height: "680px",
+  width: "1350px",
+  height: "850px",
 };
 
-let center = {
-  lat: 37.4602,
-  lng: 126.4407,
-};
-
-function MyComponent() {
+function TrudyMap() {
+  const [center, setCenter] = useState({ lat: 37.4602, lng: 126.4407 });
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyCr_VXyq_r6dte_29ocp-T2i6yf30VvUMI",
     language: "en",
   });
-
   const [map, setMap] = React.useState(null);
 
-  const onLoad = React.useCallback(function callback(map: any) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    // map.fitBounds(bounds);
-    setMap(map);
-  }, []);
+  const updateCenter = (lat: number, lng: number) => {
+    setCenter({ lat, lng });
+    console.log(center);
+  };
+
+  const onLoad = React.useCallback(
+    function callback(map: any) {
+      const bounds = new window.google.maps.LatLngBounds(center);
+      setMap(map);
+    },
+    [center]
+  );
 
   const onUnmount = React.useCallback(function callback() {
     setMap(null);
   }, []);
 
+  // useEffect(() => {
+  //   setPosition({ lat: props.latitude, lng: props.longitude });
+  //   console.log(position);
+  // }, [props.latitude, props.longitude]);
   return isLoaded ? (
     <div className="map-page-container">
       <div className="place-info">
-        <Place getPosition={center} />
+        <Place onPlaceClick={updateCenter} setCenter={setCenter} />
       </div>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14} onLoad={onLoad} onUnmount={onUnmount}>
         {/* Child components, such as markers, info windows, etc. */}
@@ -47,4 +53,4 @@ function MyComponent() {
   );
 }
 
-export default React.memo(MyComponent);
+export default React.memo(TrudyMap);
