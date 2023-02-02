3af895f504521731236d5934ada849c17e548d96
cheon2308 <cjsgusxo@gmail.com>
2023-02-03 03:31:08 +0900

    Fix : map move and not Marker

--- frontend/trudy/src/TrudyMap/TrudyMap.tsx
+++ frontend/trudy/src/TrudyMap/TrudyMap.tsx
@@ -10,6 +10,8 @@ const containerStyle = {
 
 function TrudyMap() {
   const [center, setCenter] = useState({ lat: 37.4602, lng: 126.4407 });
+  const [zoom, setZoom] = useState(14);
+  const [marker, setMarker] = useState({ lat: 37.4602, lng: 126.4407 });
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyCr_VXyq_r6dte_29ocp-T2i6yf30VvUMI",
@@ -19,13 +21,16 @@ function TrudyMap() {
 
   const updateCenter = (lat: number, lng: number) => {
     setCenter({ lat, lng });
-    console.log(center);
+    setZoom(20);
+    setMarker({ lat, lng });
+    console.log(marker);
   };
 
   const onLoad = React.useCallback(
     function callback(map: any) {
       const bounds = new window.google.maps.LatLngBounds(center);
       setMap(map);
+      setZoom(14);
     },
     [center]
   );
@@ -34,18 +39,15 @@ function TrudyMap() {
     setMap(null);
   }, []);
 
-  // useEffect(() => {
-  //   setPosition({ lat: props.latitude, lng: props.longitude });
-  //   console.log(position);
-  // }, [props.latitude, props.longitude]);
   return isLoaded ? (
     <div className="map-page-container">
       <div className="place-info">
+        <button onClick={onLoad}>reset</button>
         <Place onPlaceClick={updateCenter} setCenter={setCenter} />
       </div>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={14} onLoad={onLoad} onUnmount={onUnmount}>
-        {/* Child components, such as markers, info windows, etc. */}
-        <></>
+
+      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={zoom} onLoad={onLoad} onUnmount={onUnmount}>
+        <Marker position={marker} />
       </GoogleMap>
     </div>
   ) : (
