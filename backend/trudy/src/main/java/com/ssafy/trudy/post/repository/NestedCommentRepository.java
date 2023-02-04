0fb9dbaa9ba9715dad6fe71afcf2e35446a0aef0
pjh6947 <pjh6947@naver.com>
2023-02-04 21:32:18 +0900

    Feat : 게시글 좋아요, 댓글 작성, 대댓글 작성, 대댓글 좋아요 기능 동작

--- backend/trudy/src/main/java/com/ssafy/trudy/post/repository/NestedCommentRepository.java
+++ backend/trudy/src/main/java/com/ssafy/trudy/post/repository/NestedCommentRepository.java
@@ -1,7 +1,13 @@
 package com.ssafy.trudy.post.repository;
 
+import com.ssafy.trudy.member.model.Member;
 import com.ssafy.trudy.post.model.NestedComment;
+import com.ssafy.trudy.post.model.NestedCommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Map;
+
 public interface NestedCommentRepository extends JpaRepository<NestedComment, Long> {
+
+
 }
--- backend/trudy/src/main/java/com/ssafy/trudy/post/service/PostService.java
+++ backend/trudy/src/main/java/com/ssafy/trudy/post/service/PostService.java
@@ -1,5 +1,7 @@
 package com.ssafy.trudy.post.service;
 
+import com.ssafy.trudy.member.model.Member;
+import com.ssafy.trudy.member.repository.MemberRepository;
 import com.ssafy.trudy.post.model.*;
 import com.ssafy.trudy.post.repository.*;
 import io.jsonwebtoken.impl.crypto.MacProvider;
@@ -8,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -16,11 +19,22 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class PostService {
 
+    //post Entity 관련 Repository
     private final PostRepository postRepository;
     private final PostImageRepository postImageRepository;
     private final PostCategoryRepository postCategoryRepository;
     private final PostAreaRepository postAreaRepository;
     private final PostLikeRepository postLikeRepository;
+
+    //comment Entity 관련 Repository
+    private final CommentRepository commentRepository;
+    private final CommentLikeRepository commentLikeRepository;
+    private final NestedCommentRepository nestedCommentRepository;
+    private final NestedCommentLikeRepository nestedCommentLikeRepository;
+
+
+    //member Entity Repository
+    private final MemberRepository memberRepository;
 
     ModelMapper modelMapper = new ModelMapper();
 
@@ -99,25 +113,64 @@ public class PostService {
 
     }
 
-    //포럼 게시글 상세보기
+    //포럼 게시글 상세보기(게시글 + )
     public void /*Optional<Post>*/ findPostDetail(Long postId) throws  Exception{
 
-        /*
+        log.info("postService - findPostDetail");
+        postId = 0L;
+
+        //1. post entity를 가져옴
+        Post postEntity = postRepository.findById(postId).get();
+
+        //2. postCombine에 1개 글 상세정보(게시글, member, image, area, category, like_Count)를 채워 넣음
+        PostDto.PostCombine postCombine = new PostDto.PostCombine();
+
+        PostDto.PostElement postElement = modelMapper.map(postEntity, PostDto.PostElement.class);
+        PostDto.MemberElement memberElement = modelMapper.map(postEntity.getMemberId(), PostDto.MemberElement.class);
+
+        //image 정보 리스트 가져와서 DTO에 저장
+        List<PostDto.PostImageElement> postImageElementList = postImageRepository.findByPostId(postEntity)
+                .stream()
+                .map(p -> modelMapper.map(p, PostDto.PostImageElement.class)).collect(Collectors.toList());
+
+        //area 정보 리스트 가져와서 DTO에 저장
+        List<PostDto.PostAreaElement> postAreaElementList = postAreaRepository
+                .findByPostId(postEntity)
+                .stream()
+                .map(p -> new PostDto.PostAreaElement(
+                        modelMapper.map(p.getSigunguCode().getAreaCode(), PostDto.AreaElement.class),
+                        modelMapper.map(p.getSigunguCode(), PostDto.SigunguElement.class)
+                )).collect(Collectors.toList());
+
+        //category 정보 리스트 가져와서 DTO에 저장
+        List<PostDto.PostCategoryElement> postCategoryElementLIst = postCategoryRepository
+                .findByPostId(postEntity)
+                .stream()
+                .map(p -> modelMapper.map(p, PostDto.PostCategoryElement.class)).collect(Collectors.toList());
+
+        //postLikeCount 정보 가져옴
+        int postLikeCount = postLikeRepository.countByPostId(postEntity);
+
+        postCombine = new PostDto.PostCombine(postElement, memberElement, postImageElementList, postAreaElementList, postCategoryElementLIst, postLikeCount);
+
+
+         /*
         post entity를 가져옴
     comments entity 를 전부 구해옴
     해당 comments entity로
     -> comment_like count를 구한다, nested_comment entity를 구해옴
         -> nested_comment entity로 nested_comment like count를 구해온다
      */
-        //front에 전송할 객체
-        PostDto.CommentCombine CommentRepository = new PostDto.CommentCombine();
 
-        //post entity를 가져옴
-        Post postEntity = postRepository.findById(postId).get();
+        //3. post Entity를 이용해 댓글 정보를 채워 넣음
+        PostDto.CommentCombine commentCombine;
 
-        //postCombine 가져오는 코드 복사
+        // 댓글 정보만 채우기
+//        PostDto.CommentElement commentElement = modelMapper.map(commentRepository.findByPostId(postEntity), PostDto.CommentElement.class);
 
-        //
+        log.info("0번 글 ========= " + postCombine);
+        log.info("0번 글 댓글 ========= " + commentRepository.findByPostId(postEntity));
+
 
 
 
@@ -130,14 +183,37 @@ public class PostService {
 //        return post;
     }
 
-    //포럼 게시글 좋아요
-    public void addPostLike(){
+    //포럼 게시글 좋아요 - 정상 동작
+    public void addPostLike(Long memberId, Long postId){
+//        log.info("Service - addPostLike - memberId : postId" + memberId +" : "+postId);
+
+        //postLike Entity의 존재 확인을 위해 Member Entity와 Post Entity를 찾아온다.
+        Member memberEntity = memberRepository.findById(memberId).get();
+        Post postEntity = postRepository.findById(postId).get();
+
+        //확인
+//        log.info("Service addPostLike func - memberEntity ============== "+ memberEntity );
+//        log.info("Service addPostLike func - postEntity ============== "+ postEntity);
+
+        //member Entity와 postEntity로 구성된 postLike Entity가 있는지 확인하고, 없으면 저장하기 위한 postLikeEntity
+        PostLike postLikeEntityFind = postLikeRepository.findByMemberIdAndPostId(memberEntity, postEntity);
+        PostLike postLikeEntitySave = new PostLike(memberEntity, postEntity);
+
+        //member Entity와 postEntity로 구성된 post like가 있는지 보고 없으면 추가해주고, 있으면 지운다.
+        if(postLikeEntityFind == null) postLikeRepository.save(postLikeEntitySave);
+        else postLikeRepository.delete(postLikeEntityFind);
 
     }
 
-    //포럼 게시글 댓글 작성
-    public void addPostComment(){
+    //포럼 게시글 댓글 작성 - 정상 동작
+    public void addPostComment(Long memebrId, Long postId, String content){
+        //준비물 준비 : memebr Entity, post Entity,
+        Member memberEntity = memberRepository.findById(memebrId).get();
+        Post postEntity = postRepository.findById(postId).get();
 
+        //PostCommentRepository에 저장하기 위핸 PostComment Entity 준비 및 저장
+        Comment commentEntitySave = new Comment(postEntity, memberEntity, content, (byte) 0, LocalDateTime.now());
+        commentRepository.save(commentEntitySave);
     }
 
     //댓글 삭제
@@ -145,14 +221,30 @@ public class PostService {
 
     }
 
-    //대댓글 작성
-    public void addPostNestedComment(){
+    //대댓글 작성 - 정상 동작
+    public void addPostNestedComment(Long memebrId, Long commentId, String content){
+        //준비물 준비 : memebr Entity, comment Entity,
+        Member memberEntity = memberRepository.findById(memebrId).get();
+        Comment commentEntity = commentRepository.findById(commentId).get();
 
+        //PostCommentRepository에 저장하기 위핸 PostComment Entity 준비 및 저장
+        NestedComment nestedCommentEntitySave = new NestedComment(commentEntity, memberEntity, content, LocalDateTime.now());
+        nestedCommentRepository.save(nestedCommentEntitySave);
     }
 
-    //대댓글 좋아요
-    public void addPostNestedCommentLike(){
+    //대댓글 좋아요 - 정상 동작
+    public void addPostNestedCommentLike(Long memberId, Long nestedCommentId){
+        //NestedCommentLike entity의 존재 확인을 위해 Member entity와 NestedComment entity를 찾아온다.
+        Member memberEntity = memberRepository.findById(memberId).get();
+        NestedComment nestedCommentEntity = nestedCommentRepository.findById(nestedCommentId).get();
 
+        //member Entity와 nestedComment Entity로 구성된 nestedCommentLike Entity가 있는지 확인하고, 없으면 저장하기 위한 NestedCommentLike를 만듦
+        NestedCommentLike nestedCommentLikeFind = nestedCommentLikeRepository.findByMemberIdAndNestedCommentId(memberEntity, nestedCommentEntity);
+        NestedCommentLike nestedCommentLikeSave = new NestedCommentLike(memberEntity, nestedCommentEntity);
+
+        //Member Entity와 NestedComment로 구성된 NestedCommentLike가 있는지 보고 없으면 추가해주고, 있으면 지운다.
+        if(nestedCommentLikeFind == null) nestedCommentLikeRepository.save(nestedCommentLikeSave);
+        else nestedCommentLikeRepository.delete(nestedCommentLikeFind);
     }
 
     //대댓글 삭제
