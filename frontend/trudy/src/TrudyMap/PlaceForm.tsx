5d51468aa797f4af2cc0452fbb19f19cedb0a296
천현태[부울경_1반] <cjsgusxo@gmail.com>
2023-02-04 17:41:35 +0900

    Fix : api error

--- frontend/trudy/src/TrudyMap/PlaceForm.tsx
+++ frontend/trudy/src/TrudyMap/PlaceForm.tsx
@@ -3,10 +3,25 @@ import React, { useCallback } from "react";
 type PlaceFormProps = {
   data: {
     id: number;
-    firstImage: string;
-    title: string;
-    mapx: string;
-    mapy: string;
+  addr1: string;
+  addr2: string | undefined;
+  cat1: string;
+  cat2: string | undefined;
+  cat3: string | undefined;
+  contentId: string | undefined;
+  contentTypeId: string | undefined;
+  createdTime: string | undefined;
+  firstImage: string;
+  firstImage2: string | undefined;
+  mapx: any;
+  mapy: any;
+  title: string;
+  mlevel: string | undefined;
+  modifiedTime: string | undefined;
+  readCount: string | undefined;
+  singunguCode: string;
+  tel: string | undefined;
+  zipcode: string | undefined;
   };
   onClick: (mapx: string | number, mapy: string | number) => void;
 };
@@ -15,10 +30,10 @@ function PlaceForm({ data, onClick }: PlaceFormProps) {
   const handleClick = useCallback(() => {
     onClick(data.mapx, data.mapy);
   }, [data.mapx, data.mapy, onClick]);
-
+  console.log(data.firstImage)
   return (
     <div onClick={handleClick} style={{ display: "inline-block", cursor: "pointer" }}>
-      <img src={data.firstImage} alt="Place thumbnail" />
+      {data.firstImage ? <img src={data.firstImage} alt="Place thumbnail" /> : ''}
       <h3>{data.title}</h3>
     </div>
   );
