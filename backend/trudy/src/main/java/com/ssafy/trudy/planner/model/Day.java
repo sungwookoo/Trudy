2be48497c30739c1040dd7ea0712c93a80021979
zzangbae <bestisland01@gmail.com>
2023-02-06 15:59:25 +0900

    Feat : 플래너 수정기능 완료

--- backend/trudy/src/main/java/com/ssafy/trudy/planner/model/Day.java
+++ backend/trudy/src/main/java/com/ssafy/trudy/planner/model/Day.java
@@ -1,12 +1,9 @@
 package com.ssafy.trudy.planner.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @Data
