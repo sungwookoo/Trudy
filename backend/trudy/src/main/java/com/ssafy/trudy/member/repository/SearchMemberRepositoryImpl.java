df9db601e8f740a2b6a4fc6606c96565aa67def5
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-02-06 14:52:44 +0900

    Feat : 사용자 목록 필터

--- backend/trudy/src/main/java/com/ssafy/trudy/member/repository/SearchMemberRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.trudy.member.repository;
-
-import com.ssafy.trudy.member.model.Member;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public class SearchMemberRepositoryImpl implements SearchMemberRepository{
-
-}
