852ab1402b703c0125aae6cb53423b75802649b9
sungwookoo.dev <sungwookoo.dev@gmail.com>
2023-02-08 00:17:17 +0900

    Fix : 배포 세팅값 변경

--- frontend/trudy/src/setupProxy.js
+++ frontend/trudy/src/setupProxy.js
@@ -5,7 +5,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://localhost:8080",
+      target: "https://trudy.online",
       changeOrigin: true,
     })
   );
