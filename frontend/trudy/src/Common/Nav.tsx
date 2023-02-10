6ce095e9f632e8b8c542ff4098f091befc0fe117
EHCha <chaeh0926@gmail.com>
2023-02-10 17:14:52 +0900

    Style : MyProfile

--- frontend/trudy/src/Common/Nav.tsx
+++ frontend/trudy/src/Common/Nav.tsx
@@ -12,7 +12,6 @@ const Nav = () => {
     setIsOpen(!isOpen);
   };
 
-
   // const navigateToProfile = useNavigate()
   // navigateToProfile('/profile')
 
@@ -21,93 +20,90 @@ const Nav = () => {
     authCtx.signOut();
   };
 
-
-
-
-// 이하 HTML
+  // 이하 HTML
 
   return (
     // 네비게이션바
-  <nav className="nav-bar">
-    <div className="all-fuctions">
-      <NavLink to="/">
-        <img className="nav-logo" src={NewLogo} alt="TrudyLogo"></img>
-      </NavLink>
+    <nav className="nav-bar">
+      <div className="all-fuctions">
+        <NavLink to="/">
+          <img className="nav-logo" src={NewLogo} alt="TrudyLogo"></img>
+        </NavLink>
 
-      {/* 랜딩페이지 */}
-      {/* <div className="nav-item-logo">
+        {/* 랜딩페이지 */}
+        {/* <div className="nav-item-logo">
         <NavLink className="nav-link" to="/">
           Trudy
         </NavLink>
       </div> */}
 
-      {/* 지도 */}
-      <div className="nav-item">
-        <NavLink className="nav-link" to="/trudymap">
-          Map
-        </NavLink>
+        {/* 지도 */}
+        <div className="nav-item">
+          <NavLink className="nav-link" to="/trudymap">
+            Map
+          </NavLink>
+        </div>
+
+        {/* 포럼 */}
+        <div className="nav-item">
+          <NavLink className="nav-link" to="/forum">
+            Forum
+          </NavLink>
+        </div>
+
+        {/* 스퀘어 */}
+        <div className="nav-item">
+          <NavLink className="nav-link" to="/square">
+            Square
+          </NavLink>
+        </div>
+
+        {/* 플래너 */}
+        <div className="nav-item">
+          <NavLink className="nav-link" to="/planner">
+            Planner
+          </NavLink>
+        </div>
       </div>
 
-      {/* 포럼 */}
-      <div className="nav-item">
-        <NavLink className="nav-link" to="/forum">
-          Forum
-        </NavLink>
+      <div className="profile-signout">
+        {/* 프로필 */}
+
+        {/* 로그인 */}
+        {!authCtx.isLoggedIn ? (
+          <>
+            <div className="nav-item mx-6">
+              <NavLink className="nav-link" to="/signin">
+                Sign In
+              </NavLink>
+            </div>
+            <div className="nav-item mx-6">
+              <NavLink className="nav-link" to="/signupselect">
+                Sign Up
+              </NavLink>
+            </div>{" "}
+          </>
+        ) : (
+          <>
+            <div className="nav-item flex flex-row items-center">
+              <img
+                className="nav-profile-img mr-12"
+                src="https://blog.kakaocdn.net/dn/FSvHG/btrzdoAbEI0/WA1kfeo9BFC8n8GOe39U31/img.webp"
+                alt="navProfileImage"
+              />
+              <NavLink className="nav-link" to="/profile">
+                Profile
+              </NavLink>
+            </div>
+            <div className="nav-item">
+              <button className="nav-link ml-4 mr-12" onClick={signOut}>
+                SignOut
+              </button>
+            </div>
+          </>
+        )}
       </div>
-
-      {/* 스퀘어 */}
-      <div className="nav-item">
-        <NavLink className="nav-link" to="/square">
-          Square
-        </NavLink>
-      </div>
-
-      {/* 플래너 */}
-      <div className="nav-item">
-        <NavLink className="nav-link" to="/planner">
-          Planner
-        </NavLink>
-      </div>
-    </div>
-
-    <div className="profile-signout">
-      {/* 프로필 */}
-
-      {/* 로그인 */}
-      {!authCtx.isLoggedIn ? (
-        <>
-          <div className="nav-item mx-6">
-            <NavLink className="nav-link" to="/signin">
-              Sign In
-            </NavLink>
-          </div>
-          <div className="nav-item mx-6">
-            <NavLink className="nav-link" to="/signupselect">
-              Sign Up
-            </NavLink>
-          </div>{" "}
-        </>
-      ) : (
-        <>
-          <div className="nav-item flex flex-row items-center">
-            <img 
-            className="nav-profile-img mr-12"
-            src="https://blog.kakaocdn.net/dn/FSvHG/btrzdoAbEI0/WA1kfeo9BFC8n8GOe39U31/img.webp"
-            alt="navProfileImage"
-            />
-            <NavLink className="nav-link" to="/profile">
-              Profile
-            </NavLink>
-          </div>
-          <div className="nav-item">
-            <button className="nav-link ml-4 mr-12" onClick={signOut}>
-              Sign Out
-            </button>
-          </div>
-        </>
-      )}
-    </div>
-  </nav>
+    </nav>
   );
 };
 
